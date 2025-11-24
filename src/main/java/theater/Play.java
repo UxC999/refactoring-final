6437b850e2f676f944418cacced428af89b70a91
lisac999 <lisa.yx.chen@outlook.com>
2025-11-24 14:26:11 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,26 @@
 package theater;
 
+/**
+ * Represents a play with a name and a type (e.g., tragedy, comedy).
+ *
+ * @null This class does not accept null parameters.
+ */
+
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
