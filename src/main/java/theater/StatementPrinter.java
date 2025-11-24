05197d3724f0b420b1633e517fc37b5f0f19c5f6
lisac999 <lisa.yx.chen@outlook.com>
2025-11-24 14:59:13 -0500

    Task 2.3

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -42,7 +42,6 @@ public class StatementPrinter {
     public String statement() {
         final StringBuilder result =
                 new StringBuilder("Statement for " + invoice.getCustomer() + System.lineSeparator());
-        final NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
         int totalAmount = 0;
         int volumeCredits = 0;
@@ -56,20 +55,15 @@ public class StatementPrinter {
             result.append(String.format(
                     "  %s: %s (%s seats)%n",
                     getPlay(performance).getName(),
-                    frmt.format(thisAmount / (double) Constants.CENTS_PER_DOLLAR),
+                    usd(thisAmount),
                     performance.getAudience()
             ));
 
             totalAmount += thisAmount;
         }
 
-        result.append(String.format(
-                "Amount owed is %s%n",
-                frmt.format(totalAmount / (double) Constants.CENTS_PER_DOLLAR)
-        ));
-        result.append(String.format(
-                "You earned %s credits%n", volumeCredits
-        ));
+        result.append(String.format("Amount owed is %s%n", usd(totalAmount)));
+        result.append(String.format("You earned %s credits%n", volumeCredits));
 
         return result.toString();
     }
@@ -99,8 +93,7 @@ public class StatementPrinter {
                 result = Constants.TRAGEDY_BASE_AMOUNT;
                 if (performance.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
                     result += Constants.TRAGEDY_EXTRA_AMOUNT_PER_PERSON
-                            * (performance.getAudience()
-                            - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
+                            * (performance.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
                 }
                 break;
 
@@ -109,8 +102,7 @@ public class StatementPrinter {
                 if (performance.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
                     result += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
                             + Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
-                            * (performance.getAudience()
-                            - Constants.COMEDY_AUDIENCE_THRESHOLD);
+                            * (performance.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD);
                 }
                 result += Constants.COMEDY_AMOUNT_PER_AUDIENCE * performance.getAudience();
                 break;
@@ -139,5 +131,15 @@ public class StatementPrinter {
 
         return result;
     }
-}
 
+    /**
+     * Formats an amount in cents as US dollars.
+     *
+     * @param amount amount in cents
+     * @return formatted amount in US dollars
+     */
+    private String usd(int amount) {
+        return NumberFormat.getCurrencyInstance(Locale.US)
+                .format((double) amount / Constants.CENTS_PER_DOLLAR);
+    }
+}
